664d3b6dd1be7d09b1598d37232351eaac70b001
tahirjadoon <tjadoon@gmail.com>
2026-04-28 23:57:39 -0400

    p05-deepdive-components-traffic basic web site setup via app html, css and ts files

--- p05-deepdive-components-traffic/src/app/app.ts
+++ p05-deepdive-components-traffic/src/app/app.ts
@@ -9,4 +9,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('p05-deepdive-components-traffic');
+
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }

41ec580976445f4140e4d5dd749e06957749d896
tahirjadoon <tjadoon@gmail.com>
2026-04-30 00:22:23 -0400

    p05-deepdive-components-traffic applying styles at the component level

--- p05-deepdive-components-traffic/src/app/common/form-control/form-control.ts
+++ p05-deepdive-components-traffic/src/app/common/form-control/form-control.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-form-control',
   imports: [],
   templateUrl: './form-control.html',
   styleUrl: './form-control.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class FormControlComponent {
   label = input.required<string>();

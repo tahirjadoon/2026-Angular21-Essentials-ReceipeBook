c6643ec42edd3b9e4ea5a35f945dcce26e9d82e0
tahirjadoon <tjadoon@gmail.com>
2026-05-03 19:39:16 -0400

    p05-deepdive-components-traffic template access and ViewChild vs viewChild

--- p05-deepdive-components-traffic/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ p05-deepdive-components-traffic/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
 import { ButtonCommonComponent } from '../../../common/button-common/button-common';
 import { FormControlComponent } from '../../../common/form-control/form-control';
 
+
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonCommonComponent, FormControlComponent],
+  imports: [CommonModule, ButtonCommonComponent, FormControlComponent, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+
+  //@ViewChild('ticketForm') private ticketForm?: ElementRef<HTMLFormElement>;
+  private ticketFormSignal = viewChild.required<ElementRef<HTMLFormElement>>('ticketForm');
+
+
+  onFormSubmit(form: HTMLFormElement, titleElem: HTMLInputElement, requestElem: HTMLTextAreaElement){
+    console.log('form submit', titleElem.value, requestElem.value);
+    form.reset();
+  }
+
+  onFormSubmit2(titleElem: HTMLInputElement, requestElem: HTMLTextAreaElement){
+    console.log('form submit', titleElem.value, requestElem.value);
+    //this.ticketForm?.nativeElement.reset();
+    this.ticketFormSignal()?.nativeElement.reset();
+  }
+
+}

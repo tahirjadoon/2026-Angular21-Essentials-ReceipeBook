8165c44cceaf8deb4616e0540ca949edcea562ae
tahirjadoon <tjadoon@gmail.com>
2026-05-03 19:47:21 -0400

    p05-deepdive-components-traffic Contentchild vs contentChild signal use

--- p05-deepdive-components-traffic/src/app/common/form-control/form-control.ts
+++ p05-deepdive-components-traffic/src/app/common/form-control/form-control.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, ElementRef, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-form-control',
@@ -8,5 +8,14 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class FormControlComponent {
+  //template name variable = input
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  private control2 = contentChild.required<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+
   label = input.required<string>();
+
+  //console.log('control', this.control);
+  //console.log('control2', this.control2());
+
 }

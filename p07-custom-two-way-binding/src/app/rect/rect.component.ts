0ead383648252359a0241e5fd04d9f06aa9f9fa1
tahirjadoon <tjadoon@gmail.com>
2026-05-04 23:56:48 -0400

    p07-custom-two-way-binding easier way of custom two way binding with model() - signal

--- p07-custom-two-way-binding/src/app/rect/rect.component.ts
+++ p07-custom-two-way-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,11 +8,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  @Input({required: true}) size!: {width: string, height: string};
-  @Output() sizeChange = new EventEmitter<{width: string, height: string}>();
-
+  //@Input({required: true}) size!: {width: string, height: string};
+  //@Output() sizeChange = new EventEmitter<{width: string, height: string}>();
+  size = model.required<{width: string, height: string}>();
 
   onReset() {
-    this.sizeChange.emit({width: '200', height: '100'});
+    //this.sizeChange.emit({width: '200', height: '100'});
+    this.size.set({width: '200', height: '100'});
   }
 }

7e0c10baea056232ee8b002b6716def6c31c80df
tahirjadoon <tjadoon@gmail.com>
2026-05-07 01:25:19 -0400

    p08-directives added tooltip simple directive

--- p08-directives/src/app/learning-resources/learning-resources.component.ts
+++ p08-directives/src/app/learning-resources/learning-resources.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SafeLinkDirective } from "../directives/safe-link.directive";
 import { LogDirective } from '../directives/log.directive';
+import { TooltipDirective } from '../directives/tooltip.directive';
 
 @Component({
   selector: 'app-learning-resources',
-  imports: [SafeLinkDirective],
+  imports: [SafeLinkDirective, TooltipDirective],
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   hostDirectives:[LogDirective]

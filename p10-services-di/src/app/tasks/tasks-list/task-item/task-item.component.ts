7bbf0dc9aed7ff5b53149ab8359e1226a578487d
tahirjadoon <tjadoon@gmail.com>
2026-05-09 01:36:59 -0400

    p10-services-di changing status for the task in task-item

--- p10-services-di/src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ p10-services-di/src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,13 +1,17 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Task, TASK_STATUS, TASK_STATUS_FALLBACK_LABEL, TASK_STATUS_LABELS, TASK_STATUS_OPTIONS, TaskStatus } from '../../../common/model/task.model';
+import { TasksService } from '../../../common/services/tasks.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
+  private tasksService = inject(TasksService);
+
   task = input.required<Task>();
   taskStatusConst = TASK_STATUS;
   statusOptions = TASK_STATUS_OPTIONS;
@@ -17,7 +21,9 @@ export class TaskItemComponent {
   );
 
   onChangeTaskStatus(taskId: string, status: string) {
+    if(status === this.task().status) return;
     const newStatus: TaskStatus = status as TaskStatus;
+    this.tasksService.updateTaskStatus(taskId, newStatus);
   }
 
 

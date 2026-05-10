be4da42fc1efde4ec2706dff8ae847ad682d61e6
tahirjadoon <tjadoon@gmail.com>
2026-05-10 16:45:46 -0400

    p12-rxjs-observables interval observable

--- p12-rxjs-observables/src/app/app.ts
+++ p12-rxjs-observables/src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectorRef, Component, DestroyRef, inject, NgZone, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  
   protected readonly title = signal('p12-rxjs-observables');
+
+  interval = 0; //non signal solution
+  interval$!: Observable<number>; //observable solution
+
+  private destroyRef = inject(DestroyRef);
+
+  constructor(private zone: NgZone, private cdref: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    
+    //rxjs interval, defined in miliseconds
+    const intervalSubscription = interval(1000).subscribe({
+      next: (value) => {
+        /*
+        this.zone.run(() => {
+          this.interval = value;
+          this.cdref.detectChanges();
+        });
+        */
+        this.interval = value;
+        this.cdref.detectChanges();
+        //console.log(value);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+      error: (error) => {
+        console.log(error);
+      }
+
+    });
+    this.destroyRef.onDestroy(() => {
+      intervalSubscription.unsubscribe();
+    });
+
+
+    this.interval$ = interval(1000); //no need to unsubscribe, as the async pipe will handle it.
+  }
 }

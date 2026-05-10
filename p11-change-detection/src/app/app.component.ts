71e6e98b081bda71f7834ee92994d601ac40835f
tahirjadoon <tjadoon@gmail.com>
2026-05-10 02:49:10 -0400

    p11-change-detection project basic setup

---
+++ p11-change-detection/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+import { CounterComponent } from './counter/counter.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CounterComponent, MessagesComponent],
+})
+export class AppComponent {
+  title = signal('p11-change-detection');
+  
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
+    return 'AppComponent Component Debug Output';
+  }
+}

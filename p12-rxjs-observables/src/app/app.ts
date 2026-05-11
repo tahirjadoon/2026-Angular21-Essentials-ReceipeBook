58c7f3e025a29b732be78df0924bcbd14fb4f764
tahirjadoon <tjadoon@gmail.com>
2026-05-10 23:33:17 -0400

    p12-rxjs-observables custom observable

--- p12-rxjs-observables/src/app/app.ts
+++ p12-rxjs-observables/src/app/app.ts
@@ -14,7 +14,20 @@ export class App implements OnInit {
   protected readonly title = signal('p12-rxjs-observables');
 
   interval = 0; //non signal solution
+  interval2: string = '';
   interval$!: Observable<number>; //observable solution
+  customInterval$ = new Observable((subscriber) => {
+    let timesExexuted = 0;
+    const interval = setInterval(() => {
+      if(timesExexuted >= 5) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      } 
+      subscriber.next({message: `New value @ ${new Date()}`});
+      timesExexuted++;
+    }, 2000);
+  });
   clickCount = signal(0);
 
   private destroyRef = inject(DestroyRef);
@@ -51,8 +64,20 @@ export class App implements OnInit {
       }
 
     });
+
+    const customInterval = this.customInterval$.subscribe({
+      next: (value: any) => {
+        this.interval2 = value.message;
+        this.cdref.detectChanges();
+      },
+      complete: () => {
+        console.log('completed');
+      }
+    });
+
     this.destroyRef.onDestroy(() => {
       intervalSubscription.unsubscribe();
+      customInterval.unsubscribe();
     });
 
 

942b3506846d870b491094278280e9f56cdf41d3
tahirjadoon <tjadoon@gmail.com>
2026-05-13 19:47:54 -0400

    p15-template-driven-forms basic structure setup

--- p15-template-driven-forms/src/app/app.ts
+++ p15-template-driven-forms/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

6bac58aec306802e3c46b551c45978df8817a5d6
tahirjadoon <tjadoon@gmail.com>
2026-05-14 23:53:04 -0400

    p16-reactive-forms signup form basic setup

--- p16-reactive-forms/src/app/auth/signup/signup.component.ts
+++ p16-reactive-forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,82 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { form } from '@angular/forms/signals';
+import { mustContainQuestionMark } from '../../0common/validators-reactive/must-contain-question-mark.validator';
+import { mustMatch } from '../../0common/validators-reactive/must-match.validator';
+import { emailIsUnique } from '../../0common/validators-reactive/email-is-unique.validator';
+import { positiveNumber } from '../../0common/validators-reactive/positive-number.validator';
+import { postalCode } from '../../0common/validators-reactive/postal-code.validator';
+import { canadianAddress } from '../../0common/validators-reactive/can-address.validator';
 
 @Component({
   selector: 'app-signup',
-  standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [emailIsUnique()]
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(6), Validators.pattern(/^[A-Za-z0-9?]+$/), mustContainQuestionMark()]
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, mustMatch('password')]
+    }),
+    firstName: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    lastName: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    street: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, canadianAddress()]
+    }),
+    number: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, positiveNumber()]
+    }),
+    postalCode: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, postalCode()]
+    }),
+    city: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, canadianAddress()]
+    }),
+    role: new FormControl('', {
+      nonNullable: true,
+    }),
+    findUsGoogle: new FormControl(false),
+    findUsFriend: new FormControl(false),
+    findUsOther: new FormControl(false),
+    terms: new FormControl(false),
+  });
+
+  onSubmit() {
+    console.log("Valid: ", this.signupForm.valid,
+      " Invalid: ", this.signupForm.invalid,
+      " Touched: ", this.signupForm.touched,
+      " Untouched: ", this.signupForm.untouched,
+      " Pristine: ", this.signupForm.pristine,
+      " Dirty: ", this.signupForm.dirty,
+      " Value: ", this.signupForm.value);
+
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+
+    this.signupForm.reset();
+  }
+}

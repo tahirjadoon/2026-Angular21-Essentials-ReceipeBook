2b873b693cad2b7a0d369db88c3b8ca8caada8b4
tahirjadoon <tjadoon@gmail.com>
2026-05-14 21:04:29 -0400

    p16-reactive-forms conditionally display login and signup forms

--- p16-reactive-forms/src/app/app.ts
+++ p16-reactive-forms/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from './auth/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('p16-reactive-forms');
+
+  selectedTab = signal('login');
+
+  onTabClick(tab: string) {
+    this.selectedTab.set(tab);
+  }
 }

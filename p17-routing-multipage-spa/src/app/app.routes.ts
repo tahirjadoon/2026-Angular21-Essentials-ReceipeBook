d95858eebf0b9b5b683da7771131e75ab7cc49a5
tahirjadoon <tjadoon@gmail.com>
2026-05-18 23:45:51 -0400

    p17-routing-multipage-spa dynamic values in routes

--- p17-routing-multipage-spa/src/app/app.routes.ts
+++ p17-routing-multipage-spa/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   { path: '', component: NoTaskComponent }, //default route
-  { path: 'tasks', component: TasksComponent },
+  { path: 'users/:userId/:userName/tasks', component: UserTasksComponent },
 ];
--- p17-routing-multipage-spa/src/app/users/user/user.component.ts
+++ p17-routing-multipage-spa/src/app/users/user/user.component.ts
@@ -1,14 +1,22 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { UserData } from '../../_data/modals/user-data.model';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { UtilityService } from '../../_common/services/util.service';
 
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [RouterLink, RouterLinkActive],
 })
 export class UserComponent {
   user = input.required<UserData>();
+  utilService = inject(UtilityService);
 
   imagePath = computed(() => 'users/' + this.user().avatar);
+
+  slugify(name: string): string {
+    return this.utilService.slugify(name);
+  }
 }

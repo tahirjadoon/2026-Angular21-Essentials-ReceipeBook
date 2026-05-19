174905091c46eba0db1cee8a0f0c59d6a2d15c80
tahirjadoon <tjadoon@gmail.com>
2026-05-19 00:16:42 -0400

    p17-routing-multipage-spa reading params from routes

--- p17-routing-multipage-spa/src/app/users/user-tasks/user-tasks.component.ts
+++ p17-routing-multipage-spa/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { UserDataService } from '../../_data/services/user-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { UtilityService } from '../../_common/services/util.service';
 
 @Component({
   selector: 'app-user-tasks',
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  private userDataService = inject(UserDataService);
+  private utilService = inject(UtilityService);
+  private browserTitle = inject(Title);
+  private destroyRef = inject(DestroyRef);
+
+  //recives userId and userName as route params, userName is slugified, so we need to unslugify it before displaying
+  //two ways of reading the path parmaters, new input and observables. userName will be using observable
+  
+  //1
+  userId = input.required<string>();
+
+  //2
+  private activatedRoute = inject(ActivatedRoute);
+  
+  //1
+  userData = computed(() => {
+    const user = this.userDataService.getUserById(this.userId());
+
+    //update browser tab title
+    if(user){
+      this.browserTitle.setTitle(`p17 ${user.name} Tasks`);
+    }
+
+    return user;
+  });
+
+  ngOnInit(): void {
+    //2
+    const paramSubscription = this.activatedRoute.paramMap.subscribe({
+      next:paramMap => {
+        const userName = paramMap.get('userName');
+        console.log(this.utilService.unslugify(userName ?? ''));
+      }
+    });
+    this.destroyRef.onDestroy(() => {
+      paramSubscription.unsubscribe();
+    });
+  }
+
+
+}

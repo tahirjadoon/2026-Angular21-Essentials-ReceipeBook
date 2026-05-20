3a2c98f99193b28036841d03e2d795b15b8e68b4
tahirjadoon <tjadoon@gmail.com>
2026-05-19 22:10:06 -0400

    p17-routing-multipage-spa tasks count pill added for each user

--- p17-routing-multipage-spa/src/app/users/user/user.component.ts
+++ p17-routing-multipage-spa/src/app/users/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, input } from '@angular/core';
 import { UserData } from '../../_data/modals/user-data.model';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { UtilityService } from '../../_common/services/util.service';
+import { UserTaskDataService } from '../../_data/services/user-task-data.service';
 
 
 @Component({
@@ -14,9 +15,27 @@ export class UserComponent {
   user = input.required<UserData>();
   utilService = inject(UtilityService);
 
+  private usertaskdataService = inject(UserTaskDataService);
+
+
   imagePath = computed(() => 'users/' + this.user().avatar);
 
   slugify(name: string): string {
     return this.utilService.slugify(name);
   }
+
+  
+  taskCount = computed(() => {
+    const userData = this.user();
+    const count = this.usertaskdataService.getTasksCountByUser(userData.id) || 0;
+    //console.log(userData.id, ' = ' ,count);
+    return count;
+  });
+  
+  /*
+  taskCount = computed(() => {
+    this.usertaskdataService.tasks(); // establish dependency
+    return this.usertaskdataService.getTasksCountByUser(this.user().id);
+  });
+  */
 }

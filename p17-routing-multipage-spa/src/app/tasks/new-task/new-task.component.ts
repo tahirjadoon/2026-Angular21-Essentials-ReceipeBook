f41514c43764077dfa960885edb4cb0de6c4f5aa
tahirjadoon <tjadoon@gmail.com>
2026-05-19 21:49:47 -0400

    p17-routing-multipage-spa programmatic navigation

--- p17-routing-multipage-spa/src/app/tasks/new-task/new-task.component.ts
+++ p17-routing-multipage-spa/src/app/tasks/new-task/new-task.component.ts
@@ -2,21 +2,26 @@ import { Component, inject, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserTaskDataService } from '../../_data/services/user-task-data.service';
 import { UserTaskCreateData } from '../../_data/modals/user-task-data.mode';
+import { Router, RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   userId = input.required<string>();
+  userName = input.required<string>();
+
   enteredTitle = signal('');
   enteredSummary = signal('');
   enteredDate = signal('');
   private tasksService = inject(UserTaskDataService);
+  private router = inject(Router);
+
 
   onSubmit() {
     const newTask: UserTaskCreateData = {
@@ -26,5 +31,7 @@ export class NewTaskComponent {
     };
 
     this.tasksService.addTask(this.userId(), newTask);
+
+    this.router.navigate(['/users', this.userId(), this.userName(), 'tasks'], { replaceUrl: true } );
   }
 }

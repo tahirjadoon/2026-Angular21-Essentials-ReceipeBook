7a637e1156c3a51077ff3b35e86ec405415dc031
tahirjadoon <tjadoon@gmail.com>
2026-05-21 00:18:26 -0400

    p17-routing-multipage-spa working with query params

--- p17-routing-multipage-spa/src/app/tasks/tasks.component.ts
+++ p17-routing-multipage-spa/src/app/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
 import { UserTaskDataService } from '../_data/services/user-task-data.service';
@@ -12,7 +12,7 @@ import { map } from 'rxjs';
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent implements OnInit {
   private userTaskDataService = inject(UserTaskDataService);
@@ -34,13 +34,43 @@ export class TasksComponent implements OnInit {
   userId = input.required<string>();
   userName = input.required<string>();
 
+  order = input<'asc' | 'desc'>('desc');
+
   //userTasks: UserTaskData[] = [];
   
   //#1
   userTasks = computed(() => {
+    const order = this.order() ?? 'desc'; // fallback default
+    
     //userId is coming from the parent route
     return this.userTaskDataService.getTasksByUser(this.userId())
+    .sort((a, b) => {
+      //console.log("order=", this.order());
+      if (order === 'desc') {
+        return a.dueDate > b.dueDate ? -1 : 1;
+      } else {
+        return a.dueDate > b.dueDate ? 1 : -1;
+      }
+    });
   });
+
+  hasTasks = computed(() => {
+    const tasks = this.userTasks();
+    return tasks && tasks.length > 0;
+  });
+
+  tasksCountMoreThanOne = computed(() => {
+    const tasks = this.userTasks();
+    return tasks && tasks.length > 1;
+  });
+
+  sort = computed(() => {
+    const order = this.order();
+    return order === 'asc' ? 'desc' : 'asc';
+  })
+
+  //#2
+
 
   ngOnInit(): void {
     //2
@@ -51,8 +81,17 @@ export class TasksComponent implements OnInit {
         //this.userTasks = this.userTaskDataService.getTasksByUser(userId ?? '');
       }
     });
+
+    //alternative for query params
+    const queryParamsSubscription = this.activatedRoute.queryParams.subscribe({
+      next: queryParams => {  
+        const param = queryParams['order'];
+        console.log(param);
+      }
+    });
     this.destroyRef.onDestroy(() => {
       userIdSubscripton.unsubscribe();
+      queryParamsSubscription.unsubscribe();
     });
   }
 }
